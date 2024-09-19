23e83e755e3236b45f97271be310007017905deb
tn823 <trnghia823@gmail.com>
2024-09-19 19:36:12 +0700

    feat: loading component

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,12 @@
+import "./style/Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
