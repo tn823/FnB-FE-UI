0a7a38799e8d21a8d1c9e41ad1b033d4a16aa4db
tn823 <trnghia823@gmail.com>
2024-09-19 11:59:45 +0700

    feat: endpoint

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,6 +1,26 @@
+import burger from "../assets/burger.png"
+import topping from '../assets/topping.png'
+import kid from '../assets/kid.png'
+import drinks from '../assets/drinks.png'
+import chicken from '../assets/chicken.png'
+import salad from '../assets/salad.png'
+import cake from '../assets/cake.png'
+import breakfast from '../assets/breakfast.png'
+
 export const ROOT_URL = import.meta.env.VITE_ROOT_URL || 'http://localhost:3000/';
 
 export const ENDPOINTS = Object.freeze({
   PRODUCTS: `${ROOT_URL}api/products`,
-  // Thêm các endpoint khác ở đây
+  PRODUCTSBYCATE: `${ROOT_URL}api/products/category`
 });
+
+export const MENU_LIST = [
+  { categoryId: 605046, categoryName: "PIZZA", menu_image: burger },
+  { categoryId: 613182, categoryName: "Gà rán", menu_image: chicken },
+  { categoryId: 613739, categoryName: "Cơm", menu_image: salad },
+  { categoryId: 605964, categoryName: "RAU", menu_image: kid },
+  { categoryId: 488598, categoryName: "CAFE", menu_image: cake },
+  { categoryId: 613739, categoryName: "Cơm", menu_image: drinks },
+  { categoryId: 437160, categoryName: "NƯỚC", menu_image: breakfast },
+  { categoryId: 605545, categoryName: "BÒ", menu_image: topping },
+];
