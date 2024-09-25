77d01ff78eaca4b302b23158b7628b7eb4c8a021
tn823 <trnghia823@gmail.com>
2024-09-25 08:54:39 +0700

    update: sitebar image, sitebar name

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,10 +1,11 @@
 import burger from "../assets/burger.png"
-import topping from '../assets/topping.png'
-import kid from '../assets/kid.png'
 import drinks from '../assets/drinks.png'
 import chicken from '../assets/chicken.png'
 import salad from '../assets/salad.png'
-import cake from '../assets/cake.png'
+import combo from '../assets/combo.png'
+import pizza from '../assets/pizza.png'
+import rice from '../assets/rice.png'
+import coffee from '../assets/coffee.png'
 
 export const ROOT_URL = import.meta.env.VITE_ROOT_URL || 'http://localhost:3000/';
 
@@ -17,11 +18,12 @@ export const ENDPOINTS = Object.freeze({
 });
 
 export const MENU_LIST = [
-  { categoryId: 605046, categoryName: "PIZZA", menu_image: burger },
+  { categoryId: 605545, categoryName: "Combo", menu_image: combo },
+  { categoryId: 614862, categoryName: "Burger", menu_image: burger },
+  { categoryId: 605046, categoryName: "Pizza", menu_image: pizza },
   { categoryId: 613182, categoryName: "Gà rán", menu_image: chicken },
-  { categoryId: 613739, categoryName: "Cơm", menu_image: salad },
-  { categoryId: 605964, categoryName: "RAU", menu_image: kid },
-  { categoryId: 488598, categoryName: "CAFE", menu_image: cake },
-  { categoryId: 437160, categoryName: "NƯỚC", menu_image: drinks },
-  { categoryId: 605545, categoryName: "BÒ", menu_image: topping },
+  { categoryId: 613739, categoryName: "Cơm", menu_image: rice },
+  { categoryId: 605964, categoryName: "Salad", menu_image: salad },
+  { categoryId: 488598, categoryName: "Coffee", menu_image: coffee },
+  { categoryId: 437160, categoryName: "Nước", menu_image: drinks },
 ];
